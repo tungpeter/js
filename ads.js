4f5e9dd476fc9eba2522d014ac10149d6b0a0737
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-09 16:09:23 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,3 +1,9 @@
-function hello() {
-    alert("Hello");
-}
+<script type='text/javascript'>
+var Banner1k= new Array()
+
+Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://www.ana.co.jp/www2/travel-information/lounges/seasia-lounge-414px.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
+Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://lithaco.vn/wp-content/uploads/2021/04/pin-luu-tru-min-414x233.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
+var random=Math.round(1*Math.random());
+
+document.write(Banner1k[random]);
+</script>
