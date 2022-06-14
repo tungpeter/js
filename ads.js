a316538457783821e9234a001ad5b9309adf4e8d
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-14 20:07:58 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,6 +1,7 @@
 var Banner1k= new Array()
-Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://arobimart.com/public/userfiles/qc/banner-dan-tin_1.gif' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
-Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://arobimart.com/public/userfiles/qc/ve-sinh-cong-nghiep-tgx.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
-var random=Math.round(1*Math.random());
+Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://2.bp.blogspot.com/-xeLTa8UXEGE/WuHpaOlaC5I/AAAAAAAAJvw/7fBZVLmcaDg9tJs3rQoW03weg5ZrLCPMgCLcBGAs/s1600/rau-sach.jpg'/></a>"
+Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://1.bp.blogspot.com/-S84EPNXXOoQ/Wnk8-HoHXBI/AAAAAAAAJgw/RgnxAfJO8WIGYoj1I_9SvErKTD3g8RBcQCLcBGAs/s1600/long-chim-bien-hoa.png'/></a>"
+Banner1k[2]="<a href='http://www.tungpeter.com/'target='_blank'><img src='https://2.bp.blogspot.com/-RNxJ5_aRROE/Wi_xfkp7uPI/AAAAAAAAJPg/FmJvSXvBALMos87OwYCCaqD1kl55JXxMgCLcBGAs/s1600/tung-peter.png'/></a>"
+Banner1k[3]="<a href='http://caunoinhadat.com/nha-dat-ban/dong-nai'target='_blank'><img src='https://lh4.googleusercontent.com/-o4PhfBADnJ4/WmybmBhiCAI/AAAAAAAAJdw/-b2puYQRxAEpkHwT-M3YGS2VArpUNBKjgCLcBGAs/s1600/banner-dan-tin.gif'/></a>"
+var random=Math.round(3*Math.random());
 document.write(Banner1k[random]);
-
