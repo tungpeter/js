26c8d2532ff25d6a8edfd6ed7452733ab1eead65
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-14 15:49:58 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,6 @@
 var Banner1k= new Array()
-
 Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://arobimart.com/public/userfiles/qc/banner-dan-tin_1.gif' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
 Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://arobimart.com/public/userfiles/qc/ve-sinh-cong-nghiep-tgx.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
 var random=Math.round(1*Math.random());
-
 document.write(Banner1k[random]);
 
