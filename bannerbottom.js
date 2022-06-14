dc419b6fca6ac0d37a2f42ab4997f4f8c40c9478
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-14 18:45:43 +0700

    Create bannerbottom.js

---
+++ bannerbottom.js
@@ -0,0 +1,6 @@
+var Banner4k= new Array()
+Banner4k[0]="<a href='https://nhuongquyentoantuduy.arobiacademy.com/'target='_blank'><img src='https://1.bp.blogspot.com/-FhvKkcn9Y3Q/YHVQJM3XY-I/AAAAAAAAAbE/4WrebAhqeRwiqE3P0SWDYsc2vXH1aN6wwCLcBGAsYHQ/s16000/nhuong-quyen-thuong-hieu-smartbrain.png'/></a>"
+Banner4k[1]="<a href='https://www.lhveston.com/'target='_blank'><img src='https://caunoinhadat.com/public/userfiles/slideshow/tuyen-dai-ly-vest.png'/></a>"
+Banner4k[2]="<a href='https://nhuongquyentoantuduy.arobiacademy.com/'target='_blank'><img src='https://1.bp.blogspot.com/-FhvKkcn9Y3Q/YHVQJM3XY-I/AAAAAAAAAbE/4WrebAhqeRwiqE3P0SWDYsc2vXH1aN6wwCLcBGAsYHQ/s16000/nhuong-quyen-thuong-hieu-smartbrain.png'/></a>"
+var random=Math.round(2*Math.random());
+document.write(Banner4k[random]);
