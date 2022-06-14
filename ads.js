ae349694265fe3c8d0d30b65e8c9fde737471b29
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-14 15:46:28 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,6 +1,6 @@
 var Banner1k= new Array()
 
-Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://www.ana.co.jp/www2/travel-information/lounges/seasia-lounge-414px.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
+Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://2.bp.blogspot.com/-xWiPLhnHUU8/XDv52ESSnMI/AAAAAAAAKnM/c6z_nof5PQgDYDaMq2ondkYQyg8po8wGgCLcBGAs/s1600/sua-chua-cua-go.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
 Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://lithaco.vn/wp-content/uploads/2021/04/pin-luu-tru-min-414x233.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
 var random=Math.round(1*Math.random());
 
