ead3d8488b5df81ea208fe7462714e18406e400f
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-14 15:49:12 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,7 +1,7 @@
 var Banner1k= new Array()
 
-Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://2.bp.blogspot.com/-xWiPLhnHUU8/XDv52ESSnMI/AAAAAAAAKnM/c6z_nof5PQgDYDaMq2ondkYQyg8po8wGgCLcBGAs/s1600/sua-chua-cua-go.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
-Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://lithaco.vn/wp-content/uploads/2021/04/pin-luu-tru-min-414x233.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
+Banner1k[0]="<a href='http://www.kfarm.com.vn'target='_blank'><img src='https://arobimart.com/public/userfiles/qc/banner-dan-tin_1.gif' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
+Banner1k[1]="<a href='https://www.longchimbienhoa.com/'target='_blank'><img src='https://arobimart.com/public/userfiles/qc/ve-sinh-cong-nghiep-tgx.jpg' style='width: 98%;border-radius: 10px;margin-top: -1px;'/></a>"
 var random=Math.round(1*Math.random());
 
 document.write(Banner1k[random]);
