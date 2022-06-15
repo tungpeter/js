24a8d5089fc20fb6fc47bbfe8224e5f33964c7c4
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-15 16:10:21 +0700

    Update land.js

--- land.js
+++ land.js
@@ -1,7 +1,5 @@
-<marquee height="200" direction="UP" scrolldelay="5" scrollamount="5" onmouseover="this.stop()" onmouseout="this.start()"> 
-<center>
+document.write("
 <ul>
 <li><a href='https://www.arobistore.com/2018/10/shop-giay-nike-tai-bien-hoa.html' target='_blank' title='Shop Giày Nike Tại Biên Hòa'><span> Shop Giày Nike Tại Biên Hòa</span></a></li>
 </ul>
-</center>
-</marquee>
+");
