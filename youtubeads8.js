d8bea4b07effef7285b59a5052c49316ab09edea
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-16 10:47:01 +0700

    Update youtubeads8.js

--- youtubeads8.js
+++ youtubeads8.js
@@ -1,5 +1,5 @@
 var Banner8k= new Array()
-Banner8k[0]="<div class="g-ytsubscribe" data-channelid="UCNEIvvavn4RNP1CBv_HWPVg" data-count="default" data-layout="full"></div"
+Banner8k[0]="<div class="g-ytsubscribe" data-channelid="UCNEIvvavn4RNP1CBv_HWPVg" data-count="default" data-layout="full"></div>"
 Banner8k[1]="<div class="g-ytsubscribe" data-channelid="UCStNhVy3fz2kxTlPUVaGiQw" data-count="default" data-layout="full"></div>"
 Banner8k[2]="<div class="g-ytsubscribe" data-channelid="UC-vkvU_Sq2EveGOkfO6TNsw" data-count="default" data-layout="full"></div>"
 Banner8k[3]="<div class="g-ytsubscribe" data-channelid="UCda8ZlrhmFjYwHtlxOr_FGw" data-count="default" data-layout="full"></div>"
