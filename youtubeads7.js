b87564f99e2aeca11996b4f77d0142028afc6952
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-16 10:18:41 +0700

    Update youtubeads7.js

--- youtubeads7.js
+++ youtubeads7.js
@@ -1,7 +1,7 @@
 var Banner7k= new Array()
 Banner7k[0]="<div class="g-ytsubscribe" data-channelid="UCNEIvvavn4RNP1CBv_HWPVg" data-count="default" data-layout="full"></div"
-Banner7k[0]="<div class="g-ytsubscribe" data-channelid="UCStNhVy3fz2kxTlPUVaGiQw" data-count="default" data-layout="full"></div>"
-Banner7k[0]="<div class="g-ytsubscribe" data-channelid="UC-vkvU_Sq2EveGOkfO6TNsw" data-count="default" data-layout="full"></div>"
-Banner7k[0]="<div class="g-ytsubscribe" data-channelid="UCda8ZlrhmFjYwHtlxOr_FGw" data-count="default" data-layout="full"></div>"
-var random=Math.round(4*Math.random());
+Banner7k[1]="<div class="g-ytsubscribe" data-channelid="UCStNhVy3fz2kxTlPUVaGiQw" data-count="default" data-layout="full"></div>"
+Banner7k[2]="<div class="g-ytsubscribe" data-channelid="UC-vkvU_Sq2EveGOkfO6TNsw" data-count="default" data-layout="full"></div>"
+Banner7k[3]="<div class="g-ytsubscribe" data-channelid="UCda8ZlrhmFjYwHtlxOr_FGw" data-count="default" data-layout="full"></div>"
+var random=Math.round(3*Math.random());
 document.write(Banner7k[random]);
