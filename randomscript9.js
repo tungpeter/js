f79b3694a1aecafef53a1ba606005c45d22fd502
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-17 10:07:01 +0700

    Delete randomscript9.js

--- randomscript9.js
+++
@@ -1,5 +0,0 @@
-var Banner9k= new Array()
-Banner9k[0]="<script src="https://cdn.jsdelivr.net/gh/tungpeter/js/kgroupads.js"></script>"
-Banner9k[1]="<script src="https://cdn.jsdelivr.net/gh/tungpeter/js/longhaisecurita.js"></script>"
-var random=Math.round(1*Math.random());
-document.write(Banner9k[random]);
