69a9ee0d819a5a4100b071a998b14a968f0fa752
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-06-17 10:06:02 +0700

    Create randomscript9.js

---
+++ randomscript9.js
@@ -0,0 +1,5 @@
+var Banner9k= new Array()
+Banner9k[0]="<script src="https://cdn.jsdelivr.net/gh/tungpeter/js/kgroupads.js"></script>'/></a>"
+Banner9k[1]="<script src="https://cdn.jsdelivr.net/gh/tungpeter/js/longhaisecurita.js"></script>'/></a>"
+var random=Math.round(1*Math.random());
+document.write(Banner9k[random]);
