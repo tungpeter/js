6b2848bbc85d6393ad6297a184d192f26d42d1de
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-07-03 16:54:22 +0700

    Update bannerbottom.js

--- bannerbottom.js
+++ bannerbottom.js
@@ -1,6 +1,7 @@
 var Banner4k= new Array()
 Banner4k[0]="<a href='https://nhuongquyentoantuduy.arobiacademy.com/'target='_blank'><img src='https://1.bp.blogspot.com/-FhvKkcn9Y3Q/YHVQJM3XY-I/AAAAAAAAAbE/4WrebAhqeRwiqE3P0SWDYsc2vXH1aN6wwCLcBGAsYHQ/s16000/nhuong-quyen-thuong-hieu-smartbrain.png'/></a>"
 Banner4k[1]="<a href='https://www.lhveston.com/'target='_blank'><img src='https://caunoinhadat.com/public/userfiles/slideshow/tuyen-dai-ly-vest.png'/></a>"
-Banner4k[2]="<a href='https://nhuongquyentoantuduy.arobiacademy.com/'target='_blank'><img src='https://1.bp.blogspot.com/-FhvKkcn9Y3Q/YHVQJM3XY-I/AAAAAAAAAbE/4WrebAhqeRwiqE3P0SWDYsc2vXH1aN6wwCLcBGAsYHQ/s16000/nhuong-quyen-thuong-hieu-smartbrain.png'/></a>"
-var random=Math.round(2*Math.random());
+Banner4k[2]="<a href='https://www.onghuttrevietbac.com/'target='_blank'><img src='https://lh4.googleusercontent.com/-HVoZDcAuzAA/XVy8oERHA9I/AAAAAAAAMFc/dWr-j97aAwgh0RDKlRkaJ1E_5IaYOHmLACLcBGAs/s1600/ong-hut-tre-viet-bac.jpg'/></a>"
+Banner4k[3]="<a href='https://viettinhhoa.com.vn/'target='_blank'><img src='https://viettinhhoa.com.vn/public/userfiles/banner/banner-05-2022.jpg'/></a>"
+var random=Math.round(3*Math.random());
 document.write(Banner4k[random]);
