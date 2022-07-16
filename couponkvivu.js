fa0da5695cef840f61670e8a11ade6f867df6acc
tungpeter <102132493+tungpeter@users.noreply.github.com>
2022-07-16 08:30:06 +0700

    Update couponkvivu.js

--- couponkvivu.js
+++ couponkvivu.js
@@ -1,5 +1,5 @@
 var Banner1k= new Array()
-Banner1k[0]="<a href='https://unica.vn/?aff=27023'target='_blank'><img src='https://cdn.jsdelivr.net/gh/tungpeter/images/ma-coupon-kvivu-tai-unica.png'/></a>"
-Banner1k[1]="<a href='https://unica.vn/?aff=27023'target='_blank'><img src='https://cdn.jsdelivr.net/gh/tungpeter/images/ma-coupon-kvivu-tai-unica.png'/></a>"
+Banner1k[0]="<a href='https://unica.vn/?aff=27023'target='_blank'><img src='https://cdn.jsdelivr.net/gh/tungpeter/images/coupon-kvivu.png'/></a>"
+Banner1k[1]="<a href='https://unica.vn/?aff=27023'target='_blank'><img src='https://cdn.jsdelivr.net/gh/tungpeter/images/coupon-kvivu.png'/></a>"
 var random=Math.round(1*Math.random());
 document.write(Banner1k[random]);
