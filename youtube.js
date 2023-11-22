af6a87aa5a7080c6a030b573b23e90096a3a5491
tungpeter <102132493+tungpeter@users.noreply.github.com>
2023-11-22 09:01:37 +0700

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,5 @@
+var Banner6k= new Array()
+Banner6k[0]="<div class="g-ytsubscribe" data-channelid="UCNEIvvavn4RNP1CBv_HWPVg" data-count="default" data-layout="full"></div>"
+Banner6k[1]="<div class="g-ytsubscribe" data-channelid="UClS2yJIBDhWg6-5v0XK9gvg" data-count="default" data-layout="full"></div>"
+var random=Math.round(1*Math.random());
+document.write(Banner6k[random]);
